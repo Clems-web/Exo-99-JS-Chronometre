570218427e123c77695788032b7e7b84846a5613
Clems-web <clement.servais1@gmail.com>
2020-12-22 10:26:55 +0100

    Finito !

--- exercice1.js
+++ exercice1.js
@@ -1,12 +1,24 @@
 // Make it count up on a timer, calling this function
 let seconds = document.getElementById("seconds");
+let time = 0;
+let run = true;
 let countUp = function() {
-    // ?
+    seconds.innerHTML = time;
+    time++;
+    if (run === true) {
+        setTimeout(countUp, 1000)
+    }
 };
+countUp();
 
 // How can you make it stop counting?
+
 let stopCountUp = function() {
+    run = false;
 };
+
 let stopButton = document.getElementById("stop-button");
 stopButton.addEventListener("click", stopCountUp);
+
+
 
